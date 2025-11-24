3f76110621224a11f31d6ca5fab059a2525438db
Ömer Faruk Kocatmaz <omer.kocatmaz@sabanciuniv.edu>
2025-11-24 16:18:25 +0300

    Add files via upload

---
+++ habitflow_app/habit.dart
@@ -0,0 +1,15 @@
+class Habit {
+  String name;
+  bool isDone;
+  String? category;
+  String? frequency;
+  String? color;
+
+  Habit({
+    required this.name,
+    this.isDone = false,
+    this.category,
+    this.frequency,
+    this.color,
+  });
+}
