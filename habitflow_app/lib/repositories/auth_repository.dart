7b1b0ff736256ce6f0e39049911d10b94e920f56
Sualp Aytuğ <sualp.aytug@gmail.com>
2025-12-22 15:21:31 +0300

    Update auth_repository.dart

--- habitflow_app/lib/repositories/auth_repository.dart
+++ habitflow_app/lib/repositories/auth_repository.dart
@@ -1,21 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<User?> signUpWithEmail(String email, String password) async {
+  Future<User?> signUpWithEmail(String email, String password, String name) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return result.user;
+            
+      User? user = result.user;
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'uid': user.uid,
+          'email': email,
+          'name': name,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      }
+      
+      return user;
     } on FirebaseAuthException catch (e) {
       throw _handleAuthException(e);
     } catch (e) {
       throw 'An unexpected error occurred';
     }
   }
+
 
   Future<User?> loginWithEmail(String email, String password) async {
     try {
